b8d46d397ca0342a1bbe7da98a6c8a67952858a3
nese <yusgul716@gmail.com>
2022-02-26 13:16:40 +0300

    first

---
+++ src/day01_02_variables/C01_Variables_ay.java
@@ -0,0 +1,38 @@
+package day01_02_variables;
+
+public class C01_Variables_ay {
+
+	
+	public static void main(String[] args) {
+		
+		//variable olu�turmak ve de�er atamak farkli i�lemlerdir
+		// istersek variable olu�turma ve de�er atamayi tek satirda yapabiliriz
+		
+		String ogrenciIsim = "Celal";
+		
+		System.out.println(ogrenciIsim); //celal
+	
+	    // istersek de once olusturur, istedi�imiz zaman da deger atayabiliriz
+	int sayi;
+	sayi=10;
+	
+	System.out.println(sayi); // 10
+		
+		// biz daha once olusturulmus ve deger atamas� yapilmis degiskene 
+	    // yeni deger atayabiliriz
+	    // bu durumda java variable'in eski degerini silip yeni degeri variable'a atar
+	    sayi=25;
+	    System.out.println(sayi); // 25
+	    
+	    ogrenciIsim="Mustafa";
+		
+		System.out.println(ogrenciIsim); // Mustafa
+		
+		
+		
+		
+		
+		
+		
+	}
+}
